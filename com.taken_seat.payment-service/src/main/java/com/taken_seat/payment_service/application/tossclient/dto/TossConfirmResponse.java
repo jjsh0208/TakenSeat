14d134d0a0d6602b869369db8d5e1193d37968c7
jjsh0208 <jjsh0208@gmail.com>
2025-09-29 12:29:11 +0900

    feat : #494 Payment 정적 팩토리 메서드 생성

--- com.taken_seat.payment-service/src/main/java/com/taken_seat/payment_service/application/tossclient/dto/TossConfirmResponse.java
+++ com.taken_seat.payment-service/src/main/java/com/taken_seat/payment_service/application/tossclient/dto/TossConfirmResponse.java
@@ -1,5 +1,9 @@
 package com.taken_seat.payment_service.application.tossclient.dto;
 
+import java.time.format.DateTimeFormatter;
+
+import com.taken_seat.payment_service.domain.model.Payment;
+
 public record TossConfirmResponse(
 	String orderId,
 	String paymentKey,
@@ -11,4 +15,17 @@ public record TossConfirmResponse(
 	String idempotencyKey // 멱등키 필드 추가
 ) {
 
+	public static TossConfirmResponse from(Payment payment) {
+		// Payment 엔티티의 필드를 사용하여 TossConfirmResponse 레코드를 생성
+		return new TossConfirmResponse(
+			payment.getBookingId().toString(), // UUID -> String
+			payment.getPaymentKey(),
+			payment.getPaymentStatus().name(), // Enum -> String
+			payment.getApprovedAt() != null ? payment.getApprovedAt().format(DateTimeFormatter.ISO_DATE_TIME) : null,
+			null,
+			payment.getAmount(),
+			null,
+			payment.getIdempotencyKey()
+		);
+	}
 }
