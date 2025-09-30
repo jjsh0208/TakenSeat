1b74a69f3a9f9e595b3951d71267eec4a8fad430
jjsh0208 <jjsh0208@gmail.com>
2025-09-30 14:08:08 +0900

    feat : #494 Payment 기존의 모호한 메서드명 수정

--- com.taken_seat.payment-service/src/main/java/com/taken_seat/payment_service/application/tossclient/TossPaymentClient.java
+++ com.taken_seat.payment-service/src/main/java/com/taken_seat/payment_service/application/tossclient/TossPaymentClient.java
@@ -2,10 +2,9 @@ package com.taken_seat.payment_service.application.tossclient;
 
 import com.taken_seat.payment_service.application.tossclient.dto.TossConfirmResponse;
 import com.taken_seat.payment_service.application.tossclient.dto.TossPaymentRequest;
-import com.taken_seat.payment_service.domain.model.Payment;
 
 public interface TossPaymentClient {
 	TossConfirmResponse confirmPayment(TossPaymentRequest request, String idempotencyKey);
 
-	void refund(Payment payment, String cancelReason);
+	void cancelPayment(String paymentKey, String cancelReason, int cancelAmount, String idempotencyKey);
 }
